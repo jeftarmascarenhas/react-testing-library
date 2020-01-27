03a082cfe0c659936ea9fdd4de0fd22e7d1e9dd1
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2020-01-27 00:01:29 -0300

    finish test clickers component and update Readme

--- src/__tests__/clickers.test.js
+++ src/__tests__/clickers.test.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement,
+} from '@testing-library/react'
 
 import Clickers from '../clickers'
 
@@ -7,6 +12,18 @@ afterEach(cleanup)
 
 test('displays the count', () => {
   const { getByTestId } = render(<Clickers />)
-
   expect(getByTestId('count')).toHaveTextContent('0')
+})
+
+test('incriment count', () => {
+  const { getByTestId, getByText } = render(<Clickers />)
+  fireEvent.click(getByText(/Up/i))
+  expect(getByTestId('count')).toHaveTextContent('1')
+})
+
+test('decrement count async', async () => {
+  const { getByText } = render(<Clickers />)
+  fireEvent.click(getByText(/Down/i))
+  const countSpan = await waitForElement(() => getByText('-1'))
+  expect(countSpan).toHaveTextContent('-1')
 })
--- src/clickers.js
+++ src/clickers.js
@@ -2,8 +2,18 @@ import React, { useState } from 'react'
 
 function Clickers() {
   const [count, setCount] = useState(0)
+  const handleUpCount = () => {
+    setCount(count + 1)
+  }
+  const handleDownCount = () => {
+    setTimeout(() => {
+      setCount(count - 1)
+    }, 250)
+  }
   return (
     <div>
+      <button onClick={handleUpCount}>Up</button>
+      <button onClick={handleDownCount}>Down</button>
       <span data-testid="count">{count}</span>
     </div>
   )
